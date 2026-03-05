01cc28ebefaaed9882e51501bf135442cc3f99ff
the-silver33 <giovadesabbata@gmail.com>
2026-03-05 16:14:40 +0100

    api fix

--- api_stuff/api.js
+++ api_stuff/api.js
@@ -1,8 +1,6 @@
-import {createClient} from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://jnfqfowidqwrmmvxnvhe.supabase.co'; // API URL
 const supabaseKey = 'sb_publishable_Lbh2m9zIjUCwEF6PVNbc4g_XkdJ7Gcz'; // API key
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
