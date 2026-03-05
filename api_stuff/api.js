a57e925d9ecdce47791073b85a900c7ba8958b6c
the-silver33 <giovadesabbata@gmail.com>
2026-03-05 16:20:09 +0100

    moved api in index

--- api_stuff/api.js
+++
@@ -1,6 +0,0 @@
-const supabaseUrl = 'https://jnfqfowidqwrmmvxnvhe.supabase.co'; // API URL
-const supabaseKey = 'sb_publishable_Lbh2m9zIjUCwEF6PVNbc4g_XkdJ7Gcz'; // API key
-
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
